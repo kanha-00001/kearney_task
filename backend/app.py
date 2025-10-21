5357b78d94f2bda37ae0d0e37caa1c9642b93b25
kanha-00001 <kanhiyagoyal2003@gmail.com>
2025-10-22 03:18:51 +0530

    some new changes

--- backend/app.py
+++ backend/app.py
@@ -116,6 +116,6 @@ def query_rag_endpoint():
         db.close()
 
 if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 5000))  # <-- Render provides PORT automatically
-    app.run(host="0.0.0.0", port=port)
+    app.run(host='0.0.0.0', port=5001, debug=True)
+
 
